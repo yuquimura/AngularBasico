47392382eda8a31462ccd3f8d55777cb50bb94bc
brian <brianzambellitello@gmail.com>
2021-09-15 15:51:58 -0300

    New: Creacion de componente acerca-de.component.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
